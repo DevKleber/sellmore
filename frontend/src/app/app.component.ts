37c9d65f00d4e0804d80cb4bb5b2440cebd83fdb
kleber <kleber107@gmail.com>
2020-11-20 22:44:53 -0300

    dist

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { ThemeService } from './shared/theme/theme.service';
+import { LoginService } from './security/login/login.service';
+import { LogService } from './log/log.service';
 
 @Component({
 	selector: 'app-root',
@@ -8,9 +10,44 @@ import { ThemeService } from './shared/theme/theme.service';
 })
 export class AppComponent implements OnInit {
 	title = 'selelmore';
-	constructor(private themeService: ThemeService) {}
+	systemOff: boolean = false;
+	user: any = {};
+
+	constructor(
+		private themeService: ThemeService,
+		private loginService: LoginService,
+		private logService: LogService
+	) {}
 
 	ngOnInit(): void {
 		this.themeService.setTheme();
+		this.off();
+	}
+
+	// remover tudo
+	off() {
+		this.user = this.loginService.getUser();
+		this.saveLogAccess();
+		if (this.user.id > 6) {
+			this.systemOff = true;
+			return false;
+		}
+	}
+	saveLogAccess() {
+		if (this.user.id === 1) {
+			return;
+		}
+		let form = {
+			display_resolution: `${screen.width} x ${screen.height}`,
+			os: navigator.platform,
+			device: null,
+			language: navigator.language,
+			browser: null,
+			ip: null,
+			user_agent: navigator.userAgent,
+			id_customers: null,
+		};
+
+		this.logService.save(form);
 	}
 }
