a7e9baeca1e4b75d828008b6975ae7285790e175
fera <kleber107@gmail.com>
2020-12-03 16:26:46 -0300

    version

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { ThemeService } from './shared/theme/theme.service';
 import { LoginService } from './security/login/login.service';
+import { version } from '../../package.json';
 
 @Component({
 	selector: 'app-root',
@@ -19,5 +20,19 @@ export class AppComponent implements OnInit {
 
 	ngOnInit(): void {
 		this.themeService.setTheme();
+		this.verifyVersion();
+	}
+	verifyVersion() {
+		if (version != this.getVersionLocalStorage()) {
+			this.setVersionLocalStorage(version);
+			window.location.reload();
+		}
+	}
+
+	getVersionLocalStorage() {
+		return localStorage.getItem('wiseller_version');
+	}
+	setVersionLocalStorage(version) {
+		return localStorage.setItem('wiseller_version', version);
 	}
 }
