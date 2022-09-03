f5e4030b835860ec4aea0d0c5b708baa835804b6
g-cloud <kleber107@gmail.com>
2022-09-03 19:24:14 +0000

    commit

--- frontend-12v/src/app/pipes/nl2br.pipe.ts
+++
@@ -1,8 +0,0 @@
-import { Pipe, PipeTransform } from '@angular/core';
-
-@Pipe({ name: 'nl2br' })
-export class Nl2BrPipe implements PipeTransform {
-	transform(value: string): string {
-		return value.replace(/\n/g, '<br/>');
-	}
-}
--- frontend-12v/src/environments/environment.prod.ts
+++
@@ -1,8 +0,0 @@
-export const environment = {
-	production: true,
-
-	api: 'https://apisellmore.siagesc.com.br/api',
-	apiPublic: 'https://apisellmore.siagesc.com.br/public',
-	apiDominio: 'https://apisellmore.siagesc.com.br',
-	apiSite: 'https://apisellmore.siagesc.com.br',
-};
